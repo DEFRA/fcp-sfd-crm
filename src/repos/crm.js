939322879a690628f8f68361c1a43cf58891e44a
Fay Toward <fay.toward@defra.gov.uk>
2025-12-09 16:30:44 +0000

    implement createCase function and corresponding tests for CRM case creation

--- src/repos/crm.js
+++ src/repos/crm.js
@@ -34,8 +34,40 @@ const getAccountIdFromSbi = async (authToken, sbi) => {
     }
 }
 
-// get document type - what is this?
 // create case
+
+const createCase = async (authToken, crn, sbi) => {
+    try {
+        const { accountId } = await getAccountIdFromSbi(sbi)
+        const { contactId } = await getContactIdFromCrn(crn)
+
+        const payload = {
+            caseorigincode: 100000002,
+            casetypecode: 927350013,
+            'customerid_contact@odata.bind': `/contacts(${contactId})`,
+            'rpa_Contact@odata.bind': `/contacts(${contactId})`,
+            'rpa_Organisation@odata.bind': `/accounts(${accountId})`,
+            rpa_isunknowncontact: false,
+            rpa_isunknownorganisation: false,
+            title: 'fcp-sfd-crm test case'
+        }
+
+        const response = await fetch(`${baseUrl}/incidents`, {
+            method: 'POST',
+            headers: { Authorization: authToken, 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload)
+        })
+
+        console.log(response.headers.get('location'), '*******')
+    } catch (err) {
+        console.error(err)
+    }
+    return {
+        caseId: 'test'
+    }
+}
+
+// get document type - what is this?
 // create online submission (params dictate type)
 
-export { getContactIdFromCrn, getAccountIdFromSbi }
+export { getContactIdFromCrn, getAccountIdFromSbi, createCase }
--- test/integration/create-case.test.js
+++ test/integration/create-case.test.js
@@ -1,10 +1,11 @@
 import { describe, test, expect } from 'vitest'
 import { getCrmAuthToken } from '../../src/auth/get-crm-auth-token'
-import { getContactIdFromCrn, getAccountIdFromSbi } from '../../src/repos/crm'
+import { getContactIdFromCrn, getAccountIdFromSbi, createCase } from '../../src/repos/crm'
 
 describe('Create case in CRM', () => {
+    const validCrn = 2624714378
+    const validSbi = 155363499
     describe('GET contactId from crn', () => {
-        const validCrn = 2624714378
 
         test('should return the contactId when crn exists in CRM', async () => {
             const authToken = await getCrmAuthToken()
@@ -15,13 +16,20 @@ describe('Create case in CRM', () => {
     })
 
     describe('GET accountId from sbi', () => {
-        const validSbi = 155363499
-
         test('should return the accountId when sbi exists in CRM', async () => {
             const authToken = await getCrmAuthToken()
             const { accountId } = await getAccountIdFromSbi(authToken, validSbi)
             expect(accountId).toBeDefined()
             expect(accountId).toBe('4ab7b45b-aba2-f011-bbd2-7ced8d4645a2')
+        })
+    })
+
+    describe('POST Create case in CRM', () => {
+        test('should return the caseId when a case is successfully created.', async () => {
+            const authToken = await getCrmAuthToken()
+            const { caseId } = await createCase(authToken, validCrn, validSbi)
+
+            expect(caseId).toBeDefined()
         })
     })
 })
