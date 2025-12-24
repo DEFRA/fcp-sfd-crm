8a6ca706bb63fd5c76dff659948bb7ea2c1fa132
Natacha <natacha.tomkinson@defra.gov.uk>
2025-12-24 10:29:50 +0000

    chore(#sfd2-600): add temporary route for testing (#6)
    * chore(#sfd2-600): add temporary route for testing
    
    * test(#sfd2-600): add test for temporary route
    
    * chore(#sfd2-600): codereview - update 401 in favour of node http constant
    
    * chore(#sfd2-600): codereview - rename api-key to be more meaningful
    
    * chore(#sfd2-600): apply sonarqube's recommendations regarding unused params and consistent return

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import path from 'node:path'
 import hapi from '@hapi/hapi'
-
+import http2 from 'node:http2'
+import Joi from 'joi'
 import { config } from './config/index.js'
 import { requestLogger } from './logging/request-logger.js'
 import { secureContext } from './api/common/helpers/secure-context/secure-context.js'
 import { pulse } from './api/common/helpers/pulse.js'
 import { requestTracing } from './api/common/helpers/request-tracing.js'
 import { setupProxy } from './api/common/helpers/proxy/setup-proxy.js'
+
+const { constants: httpConstants } = http2
 
 const createServer = async () => {
   setupProxy()
@@ -44,6 +47,41 @@ const createServer = async () => {
     secureContext,
     pulse
   ])
+
+  if (config.get('cdpEnvironment') !== 'prod') {
+    server.route([
+      {
+        method: 'POST',
+        path: '/create-case',
+        options: {
+          validate: {
+            headers: Joi.object({
+              'x-api-key': Joi.string().valid(config.get('apiKeyForTestingCaseCreation')).required()
+            }).unknown(),
+            failAction: async function (_request, h, error) {
+              const headerError = Array.isArray(error?.details) &&
+                error.details.some(d => d?.context?.key === 'x-api-key')
+              if (headerError) {
+                return h
+                  .response({ error: 'Missing or invalid QA-specific x-api-key header' })
+                  .code(httpConstants.HTTP_STATUS_UNAUTHORIZED)
+                  .takeover()
+              }
+
+              return h.continue
+            }
+          },
+          handler: async (request) => {
+            const { getCrmAuthToken } = await import('./auth/get-crm-auth-token.js')
+            const { createCaseInCrm } = await import('./services/create-case-in-crm.js')
+            const authToken = await getCrmAuthToken()
+            const caseResult = await createCaseInCrm({ authToken, caseData: request.payload })
+            return { caseResult }
+          }
+        }
+      }
+    ])
+  }
 
   return server
 }
