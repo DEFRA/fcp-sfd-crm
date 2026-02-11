12828656d7327ae67ed3e0feeb4917fd35a4d44d
Rana Salem <ranatasalem@gmail.com>
2026-02-11 16:14:31 +0000

    update publishReceivedEvent

--- src/messaging/outbound/received-event/publish-received-event.js
+++ src/messaging/outbound/received-event/publish-received-event.js
@@ -3,7 +3,6 @@ import { config } from '../../../config/index.js'
 import { snsClient } from '../../sns/client.js'
 import { publish } from '../../sns/publish.js'
 import { buildReceivedEvent } from './build-received-event.js'
-import { eventToTypeMap } from '../../../constants/events.js'
 
 const snsTopic = config.get('messaging.crmEvents.topicArn')
 
@@ -17,23 +16,7 @@ export const publishReceivedEvent = async (message) => {
     throw new Error('CloudEvent type is required to publish CRM event')
   }
 
-  const mappedCaseType = eventToTypeMap[type]
-
-  if (!mappedCaseType) {
-    logger.warn(`Unmapped CloudEvent type: ${type}. Using default: 'unknown-case-type'`)
-  }
-
-  const caseType = mappedCaseType || 'unknown-case-type'
-
-  const normalisedMessage = {
-    ...message,
-    data: {
-      ...message.data,
-      caseType
-    }
-  }
-
-  const receivedRequest = buildReceivedEvent(normalisedMessage, type)
+  const receivedRequest = buildReceivedEvent(message, type)
 
   try {
     await publish(snsClient, snsTopic, receivedRequest)
