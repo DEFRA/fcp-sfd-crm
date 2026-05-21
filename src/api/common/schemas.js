8bfd9b514e83b16244e2a041326c24a7ba9606f3
Fay Toward <fay.toward@defra.gov.uk>
2026-05-21 15:39:36 +0100

    refactor: remove unused createCasePayloadSchema definition and simplify failAction parameter

--- src/api/common/schemas.js
+++ src/api/common/schemas.js
@@ -1,27 +1,5 @@
 import Joi from 'joi'
-
-export const createCasePayloadSchema = Joi.object({
-    caseType: Joi.string().required(),
-    crn: Joi.string().required(),
-    sbi: Joi.string().required(),
-    caseData: Joi.object({
-        title: Joi.string().required(),
-        caseDescription: Joi.string().required()
-    }).required(),
-    onlineSubmissionActivity: Joi.object({
-        subject: Joi.string().required(),
-        description: Joi.string().required(),
-        scheduledStart: Joi.string().isoDate().required(),
-        scheduledEnd: Joi.string().isoDate().required(),
-        stateCode: Joi.number().required(),
-        statusCode: Joi.number().required(),
-        metadata: Joi.object({
-            name: Joi.string().required(),
-            documentType: Joi.string().required(),
-            blobFileId: Joi.string().required()
-        }).required()
-    }).required()
-}).required()
+import { createCasePayloadSchema } from '../schemas/http.js'
 
 // Inbound CloudEvents payload schema (expects CloudEvents wrapper with data)
 export const inboundCloudEventSchema = Joi.object({
