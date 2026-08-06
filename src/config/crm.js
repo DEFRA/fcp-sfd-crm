49f33f171966496713b6a48281571becebee2d57
Natacha <natacha.tomkinson@defra.gov.uk>
2026-08-06 10:23:24 +0100

    chore(fls1-138): bump timeout for case creation (takes an average of ~12seconds!) and fix vulnerabilities (#130) #patch

--- src/config/crm.js
+++ src/config/crm.js
@@ -5,6 +5,12 @@ export const crmConfig = {
       format: String,
       default: null,
       env: 'CRM_API_BASE_URL'
+    },
+    caseOriginCode: {
+      doc: 'Case origin code applied to CRM cases (incidents).',
+      format: Number,
+      default: 3,
+      env: 'CRM_CASE_ORIGIN_CODE'
     }
   }
 }
