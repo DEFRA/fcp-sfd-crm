bf146cec81273ba9caf6a5ca44acd11936da0c0a
Natacha Tomkinson <natacha.tomkinson@defra.gov.uk>
2026-08-20 15:20:31 +0100

    chore(#fls1-152): add unit test for new config

---
+++ test/unit/constants/case-creation-metrics.test.js
@@ -0,0 +1,30 @@
+import { describe, test, expect } from 'vitest'
+import { caseCreationMetrics } from '../../../src/constants/case-creation-metrics.js'
+
+describe('src/constants/case-creation-metrics.js', () => {
+  test('exposes the case creation metrics literals', () => {
+    expect(caseCreationMetrics).toEqual({
+      WAITING_FOR_CASE: 'crm.case.waiting_for_case',
+      CREATOR_ROLE_CLAIMED: 'crm.case.creator_role_claimed',
+      CREATOR_ROLE_RELEASED: 'crm.case.creator_role_released'
+    })
+  })
+
+  test('WAITING_FOR_CASE metric is correctly defined', () => {
+    expect(caseCreationMetrics.WAITING_FOR_CASE).toBe('crm.case.waiting_for_case')
+  })
+
+  test('CREATOR_ROLE_CLAIMED metric is correctly defined', () => {
+    expect(caseCreationMetrics.CREATOR_ROLE_CLAIMED).toBe('crm.case.creator_role_claimed')
+  })
+
+  test('CREATOR_ROLE_RELEASED metric is correctly defined', () => {
+    expect(caseCreationMetrics.CREATOR_ROLE_RELEASED).toBe('crm.case.creator_role_released')
+  })
+
+  test('all metrics follow the naming convention', () => {
+    Object.values(caseCreationMetrics).forEach((metric) => {
+      expect(metric).toMatch(/^crm\.case\./)
+    })
+  })
+})
